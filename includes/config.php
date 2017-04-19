0052620104a27e78d8335d30b5f829630878eb7a
Robin Bohnen <robin@bohnen.nl>
2017-04-19 13:23:13 +0200

    Delete config.php

--- includes/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-DEFINE("API_URL", 	"http://mijn-ips.test.are.builders/api/");
-DEFINE("API_VERSION",	"v1");
-DEFINE("API_USER", 	"testgebruiker@ips.nl");
-DEFINE("API_PASS",	"co7UBJP6jWwG");
