752f7e5e96f16ed720224799795266f2a1b7a4e0
Robin Bohnen <r.bohnen@ips.nl>
2017-04-20 07:07:22 +0200

    Tidy of files

--- classes/class.ipsapi.php
+++ classes/class.ipsapi.php
@@ -1,73 +1,79 @@
 <?php
 
-require_once("includes/config.php");
+require_once "includes/config.php";
 
 class IPS_Api {
 
-	public function __construct() {
+  public function __construct() {
 
-		$this->connect();
+    $this->connect();
 
-	}
+  }
 
-	private function connect() {
-		$login = $this->_call("GET","/authenticate");
-		return $login;
-	}
-	
-	public function Clients_GetAll() {
-		
-		$clients = $this->_call("GET","/clients");
-		return $clients;
 
-	}
+  private function connect() {
+    $login = $this->_call("GET", "/authenticate");
+    return $login;
+  }
 
-	public function Domains_GetAll() {
 
-		$domains = $this->_call("GET","/domains");
-		return $domains;
-	
-	}
+  public function Clients_GetAll() {
 
-	public function Domains_Autoresponders_GetAll($domainname) {
+    $clients = $this->_call("GET", "/clients");
+    return $clients;
 
-		if (!isset($domainname) OR empty($domainname)) {
-			return false;
-		}
-		else {
-			$autoresponders = $this->_call("GET","/domains/".$domainname."/mail_autoresponders/");
-			return $autoresponders;
-		}
+  }
 
-	}
 
-	private function _call($method, $url, $data = false) {
-	
-		$curl = curl_init();
-		curl_setopt_array($curl, array(
-			CURLOPT_URL => API_URL.API_VERSION.$url,
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_ENCODING => "",
-			CURLOPT_MAXREDIRS => 10,
-			CURLOPT_TIMEOUT => 30,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => $method,
-			CURLOPT_HTTPHEADER => array(
-				"authorization: Basic ".base64_encode(API_USER.":".API_PASS).""
-			),
-		));
+  public function Domains_GetAll() {
 
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
+    $domains = $this->_call("GET", "/domains");
+    return $domains;
 
-		curl_close($curl);
-		
-		if ($err) {
-			return false;
-		} else {
-			return json_decode($response,true);
-		}
+  }
 
-	}
+
+  public function Domains_Autoresponders_GetAll($domainname) {
+
+    if (!isset($domainname) or empty($domainname)) {
+      return false;
+    }
+    else {
+      $autoresponders = $this->_call("GET", "/domains/".$domainname."/mail_autoresponders/");
+      return $autoresponders;
+    }
+
+  }
+
+
+  private function _call($method, $url, $data = false) {
+
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => API_URL.API_VERSION.$url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => $method,
+        CURLOPT_HTTPHEADER => array(
+          "authorization: Basic ".base64_encode(API_USER.":".API_PASS).""
+        ),
+      ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+      return false;
+    } else {
+      return json_decode($response, true);
+    }
+
+  }
+
 
 }
