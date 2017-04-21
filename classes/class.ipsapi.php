b2dd3d62a8cb269637bc6e806087121e4dfcb809
Robin Bohnen <r.bohnen@ips.nl>
2017-04-21 07:38:30 +0200

    More functions to update, delete and add object for IPS API added

--- classes/class.ipsapi.php
+++ classes/class.ipsapi.php
@@ -58,7 +58,7 @@ class IPS_Api {
 
   public function Domains_Autoresponders_GetOne($domainname, $autoresponder) {
 
-    if (!isset($domainname) or empty($domainname) or !isset($autoresponder) or empty($autoresponder)) {
+    if (!isset($domainname, $autoresponder) or empty($domainname) or empty($autoresponder)) {
       return false;
     }
     else {
@@ -84,12 +84,56 @@ class IPS_Api {
 
   public function Domains_DNS_GetOne($domainname, $record_id) {
 
-    if (!isset($domainname) or empty($domainname) or !isset($record_id) or empty($record_id) or !is_numeric($record_id)) {
+    if (!isset($domainname, $record_id) or empty($domainname) or empty($record_id) or !is_numeric($record_id)) {
       return false;
     }
     else {
       $dnsrecords = $this->_call("GET", "/domains/".$domainname."/dns_records/".$record_id);
       return $dnsrecords;
+    }
+
+  }
+
+
+  public function Domains_DNS_AddRecord($domainname, $name, $type, $content, $ttl, $prio = null) {
+
+    if (!isset($domainname, $name, $type, $content, $ttl) or empty($domainname) or empty($name) or empty($type) or empty($content) or empty($ttl)) {
+      return false;
+    }
+    else {
+      $postdata = array("name" => $name, "type" => $type, "content" => $content, "ttl" => $ttl);
+
+      $dnsrecord = $this->_call("POST", "/domains/".$domainname."/dns_records", json_encode($postdata));
+      return $dnsrecord;
+    }
+
+  }
+
+
+  public function Domains_DNS_UpdateRecord($domainname, $record_id, $name, $content, $ttl, $prio = null) {
+
+    if (!isset($domainname, $record_id, $name, $content, $ttl) or empty($domainname) or empty($record_id) or empty($name) or empty($content) or empty($ttl) or !is_numeric($record_id)) {
+      return false;
+    }
+    else {
+      $postdata = array("name" => $name, "content" => $content, "ttl" => $ttl, "prio" => $prio);
+
+      $dnsrecord = $this->_call("PUT", "/domains/".$domainname."/dns_records/".$record_id, json_encode($postdata));
+      return $dnsrecord;
+    }
+
+  }
+
+
+  public function Domains_DNS_DeleteRecord($domainname, $record_id) {
+
+    if (!isset($domainname, $record_id) or empty($domainname) or empty($record_id)) {
+      return false;
+    }
+    else {
+
+      $dnsrecord = $this->_call("DELETE", "/domains/".$domainname."/dns_records/".$record_id, json_encode($postdata));
+      return $dnsrecord;
     }
 
   }
@@ -110,7 +154,7 @@ class IPS_Api {
 
   public function Domains_Databases_GetOne($domainname, $database) {
 
-    if (!isset($domainname) or empty($domainname) or !isset($database) or empty($database)) {
+    if (!isset($domainname, $database) or empty($domainname) or empty($database)) {
       return false;
     }
     else {
@@ -136,11 +180,54 @@ class IPS_Api {
 
   public function Domains_FTPUsers_GetOne($domainname, $ftpuser) {
 
-    if (!isset($domainname) or empty($domainname) or !isset($ftpuser) or empty($ftpuser)) {
+    if (!isset($domainname, $ftpuser) or empty($domainname) or empty($ftpuser)) {
       return false;
     }
     else {
       $ftpusers = $this->_call("GET", "/domains/".$domainname."/ftp_users/".$ftpuser);
+      return $ftpusers;
+    }
+
+  }
+
+
+  public function Domains_FTPUsers_AddUser($domainname, $ftpuser, $ftppass, $type = 'domain') {
+
+    if (!isset($domainname, $ftpuser, $ftppass) or empty($domainname) or empty($ftpuser) or empty($ftppass)) {
+      return false;
+    }
+    else {
+      $postdata = array("account" => $ftpuser, "password" => $ftppass, "type" => $type);
+
+      $ftpusers = $this->_call("POST", "/domains/".$domainname."/ftp_users", json_encode($postdata));
+      return $ftpusers;
+    }
+
+  }
+
+
+  public function Domains_FTPUsers_UpdateUser($domainname, $ftpuser, $ftppass) {
+
+    if (!isset($domainname, $ftpuser, $ftppass) or empty($domainname) or empty($ftpuser) or empty($ftppass)) {
+      return false;
+    }
+    else {
+      $postdata = array("password" => $ftppass);
+
+      $ftpusers = $this->_call("PUT", "/domains/".$domainname."/ftp_users/".$ftpuser, json_encode($postdata));
+      return $ftpusers;
+    }
+
+  }
+
+
+  public function Domains_FTPUsers_DeleteUser($domainname, $ftpuser) {
+
+    if (!isset($domainname, $ftpuser) or empty($domainname) or empty($ftpuser)) {
+      return false;
+    }
+    else {
+      $ftpusers = $this->_call("DELETE", "/domains/".$domainname."/ftp_users/".$ftpuser);
       return $ftpusers;
     }
 
@@ -162,7 +249,7 @@ class IPS_Api {
 
   public function Domains_MailForwards_GetOne($domainname, $mailforward) {
 
-    if (!isset($domainname) or empty($domainname) or !isset($mailforward) or empty($mailforward)) {
+    if (!isset($domainname, $mailforward) or empty($domainname) or empty($mailforward)) {
       return false;
     }
     else {
@@ -188,7 +275,7 @@ class IPS_Api {
 
   public function Domains_Mailboxes_GetOne($domainname, $mailbox) {
 
-    if (!isset($domainname) or empty($domainname) or !isset($mailbox) or empty($mailbox)) {
+    if (!isset($domainname, $mailbox) or empty($domainname) or empty($mailbox)) {
       return false;
     }
     else {
@@ -197,21 +284,23 @@ class IPS_Api {
     }
 
   }
-  
-  
+
+
   public function Products_SharedHosting_GetAll() {
 
     $sharedhostingproducts = $this->_call("GET", "/products/sharedhosting");
     return $sharedhostingproducts;
 
   }
-  
+
+
   public function Products_SSLCertificates_GetAll() {
 
     $sslcertificates = $this->_call("GET", "/ssl_certificates/types");
     return $sslcertificates;
 
   }
+
 
   public function Domains_Check_Top5($domainname) {
 
@@ -224,7 +313,8 @@ class IPS_Api {
     }
 
   }
-  
+
+
   public function Domains_Check_Top15($domainname) {
 
     if (!isset($domainname) or empty($domainname)) {
@@ -236,15 +326,16 @@ class IPS_Api {
     }
 
   }
-  
+
+
   public function SSLCertificates_GetAll() {
 
     $sslcertificates = $this->_call("GET", "/ssl_certificates");
     return $sslcertificates;
 
   }
-  
-  
+
+
   public function SSLCertificates_GetOne($record_id) {
 
     if (!isset($record_id) or empty($record_id) or !is_numeric($record_id)) {
@@ -256,16 +347,18 @@ class IPS_Api {
     }
 
   }
-  
+
 
   private function _call($method, $url, $data = false) {
 
     if ($this->token === null) {
-      $login = "authorization: Basic ".base64_encode(API_USER.":".API_PASS)."\n";
+      $login = "authorization: Basic ".base64_encode(API_USER.":".API_PASS);
     }
     else {
-      $login = "authorization: ".$this->token."\n";
+      $login = "authorization: ".$this->token;
     }
+
+    print_r($data);
 
     $curl = curl_init();
     curl_setopt_array($curl, array(
@@ -276,8 +369,10 @@ class IPS_Api {
         CURLOPT_TIMEOUT => 30,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => $method,
+        CURLOPT_POSTFIELDS => $data,
         CURLOPT_HTTPHEADER => array(
-          $login
+          $login,
+          "content-type: application/json"
         ),
       ));
 
